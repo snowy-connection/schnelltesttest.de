c026c4a972bb0c129b883ae0fddd774344a896b0
Joshua K <kwjoshua@gmail.com>
2022-02-01 20:53:33 +0100

    Add alt-text to a test's result icon

--- src/components/Result/ResultIcon.tsx
+++ src/components/Result/ResultIcon.tsx
@@ -6,7 +6,14 @@ export function ResultIcon({ passed }: { passed: boolean }): JSX.Element {
   // TODO: Icons as SVG
   return (
     <>
-      <img src={passed ? passedIcon : failedIcon} />
+      <img
+        src={passed ? passedIcon : failedIcon}
+        alt={
+          passed
+            ? 'Logo mit weißem X auf rotem Grund als Signal für einen wenig aussagekräftigen Test'
+            : 'Logo mit weißem Haken auf grünem Grund als Signal für einen aussagekräftigen Test'
+        }
+      />
     </>
   );
 }
